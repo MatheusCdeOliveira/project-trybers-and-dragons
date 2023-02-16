fcba2834c0b4d46f2fc0dab9fcbc0edf8dc59238
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-02-16 15:22:45 -0300

    req 01 done

---
+++ src/Races/Race.ts
@@ -0,0 +1,22 @@
+abstract class Race {
+  constructor(
+    private _name: string,
+    private _dexterity: number,
+  ) {}
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+}
+
+export default Race;
